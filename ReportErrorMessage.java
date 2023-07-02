19b0912461ff689317c09984ec3d41f00baa9c5b
WoefulMetal643 <138256039+WoefulMetal643@users.noreply.github.com>
2023-07-02 14:03:50 -0700

    ReportErrorMessage

---
+++ ReportErrorMessage.java
@@ -0,0 +1,9 @@
+import javafx.application.Application;
+
+import javafx.scene.control.Label;
+
+public class ReportErrorMessage extends WarningSystem {
+	public ReportErrorMessage(String messageContent) {
+		super(messageContent);
+	}
+}
